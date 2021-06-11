9483a650dd1ac8b5445cadb533207ab7ba60a289
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-06-11 13:13:01 -0500

    added fields to models, syncing to database

--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreign_key: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "products_with_tag",
+  foreign_key: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "assigned_tags",
+  foreign_key: "tag_id",
+});
 
 module.exports = {
   Product,
